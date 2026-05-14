540aa3b203fe774d28944b716068b62f5fecc182
strayrampowtf <strayrampo@gmail.com>
2026-05-14 12:34:16 +0300

    applied clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 // Точка входа в программу
 int main(){
+=======
+// Entry point of the program
+int
+main()
+{
+>>>>>>> 04e31ec (applied clang-format Mozilla style)
     std::string name;
    // Читаем имя пользователя со стандартного ввода
     std::cin >> name;
@@ -10,3 +17,5 @@ int main(){
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
+
+
